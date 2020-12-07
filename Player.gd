c080bff81f0b549e83b7bf7f1ad60e6d72433c5c
zygelon <zygelon@gmail.com>
2020-12-07 02:50:34 +0200

    added movement

--- Player.gd
+++ Player.gd
@@ -12,9 +12,35 @@ var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	 # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity=Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x +=1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x-=1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y+=1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y-=1
+	if velocity.length()>0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position+=velocity*delta
+	position.x=clamp(position.x, 0, screen_size.x)
+	position.y=clamp(position.y,0,screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite.animation='walk'
+		$AnimatedSprite.flip_v=false
+		$AnimatedSprite.flip_h=velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = 'up'
+		$AnimatedSprite.flip_v = velocity.y > 0
+		
+	
